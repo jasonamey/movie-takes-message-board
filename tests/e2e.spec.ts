3e363b989a56e1a247b53b9ad94253d91712c67f
Jason Amey <jason.amey@gmail.com>
2023-08-28 08:27:47 -0400

    change tests

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -3,44 +3,44 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const authSetup = async (page: Page) => {
-  const browser = await firefox.launch({
-    headless: false,
-  });
-  const context = await browser.newContext();
-  await page.goto("/");
-  await page.getByRole("button", { name: "Log In" }).click();
-  await page
-    .getByLabel("Email or phone")
-    .fill(process.env.PLAYWRIGHT_EMAIL as string);
-  await page.getByLabel("Email or phone").press("Enter");
-  await page
-    .getByLabel("Enter your password")
-    .fill(process.env.PLAYWRIGHT_PWD as string);
-  await page.getByLabel("Enter your password").press("Enter");
-  await page.getByRole("button", { name: "Sign in with Google" }).click();
-  return { browser, context };
-};
+// const authSetup = async (page: Page) => {
+//   const browser = await firefox.launch({
+//     headless: false,
+//   });
+//   const context = await browser.newContext();
+//   await page.goto("/");
+//   await page.getByRole("button", { name: "Log In" }).click();
+//   await page
+//     .getByLabel("Email or phone")
+//     .fill(process.env.PLAYWRIGHT_EMAIL as string);
+//   await page.getByLabel("Email or phone").press("Enter");
+//   await page
+//     .getByLabel("Enter your password")
+//     .fill(process.env.PLAYWRIGHT_PWD as string);
+//   await page.getByLabel("Enter your password").press("Enter");
+//   await page.getByRole("button", { name: "Sign in with Google" }).click();
+//   return { browser, context };
+// };
 
-test("auth test", async ({ page }) => {
-  const browser = await firefox.launch({
-    headless: false,
-  });
-  const context = await browser.newContext();
-  await page.goto("/");
-  await page.getByRole("button", { name: "Log In" }).click();
-  await page
-    .getByLabel("Email or phone")
-    .fill(process.env.PLAYWRIGHT_EMAIL as string);
-  await page.getByLabel("Email or phone").press("Enter");
-  await page
-    .getByLabel("Enter your password")
-    .fill(process.env.PLAYWRIGHT_PWD as string);
-  await page.getByLabel("Enter your password").press("Enter");
-  await page.getByRole("button", { name: "Sign in with Google" }).click();
-  await context.close();
-  await browser.close();
-});
+// test("auth test", async ({ page }) => {
+//   const browser = await firefox.launch({
+//     headless: false,
+//   });
+//   const context = await browser.newContext();
+//   await page.goto("/");
+//   await page.getByRole("button", { name: "Log In" }).click();
+//   await page
+//     .getByLabel("Email or phone")
+//     .fill(process.env.PLAYWRIGHT_EMAIL as string);
+//   await page.getByLabel("Email or phone").press("Enter");
+//   await page
+//     .getByLabel("Enter your password")
+//     .fill(process.env.PLAYWRIGHT_PWD as string);
+//   await page.getByLabel("Enter your password").press("Enter");
+//   await page.getByRole("button", { name: "Sign in with Google" }).click();
+//   await context.close();
+//   await browser.close();
+// });
 
 test("see that app is rendering", async ({ page }) => {
   const browser = await firefox.launch({
@@ -62,42 +62,42 @@ test("see that app is rendering", async ({ page }) => {
   ).toBeVisible();
 });
 
-test("see that takes are rendering", async ({ page }) => {
-  await page.goto("/");
-  await expect(
-    page.getByRole("link", {
-      name: 'Arrival (2016) @jason.amey\'s take: "a movie kind-of about everything" Sci-Fi',
-    })
-  ).toBeVisible();
-});
+// test("see that takes are rendering", async ({ page }) => {
+//   await page.goto("/");
+//   await expect(
+//     page.getByRole("link", {
+//       name: 'Arrival (2016) @jason.amey\'s take: "a movie kind-of about everything" Sci-Fi',
+//     })
+//   ).toBeVisible();
+// });
 
-test("see that app is rendering again", async ({ page }) => {
-  await page.goto("/");
-  await page.getByRole("button", { name: "Log In" }).click();
-  await page.getByLabel("Email or phone").click();
-  await page.getByLabel("Email or phone").fill("");
-  await page.getByLabel("Email or phone").press("Enter");
-  await page.getByLabel("Enter your password").fill("");
-  await page.getByLabel("Enter your password").press("Enter");
-  await page.getByRole("button", { name: "Sign in with Google" }).click();
-});
+// test("see that app is rendering again", async ({ page }) => {
+//   await page.goto("/");
+//   await page.getByRole("button", { name: "Log In" }).click();
+//   await page.getByLabel("Email or phone").click();
+//   await page.getByLabel("Email or phone").fill("");
+//   await page.getByLabel("Email or phone").press("Enter");
+//   await page.getByLabel("Enter your password").fill("");
+//   await page.getByLabel("Enter your password").press("Enter");
+//   await page.getByRole("button", { name: "Sign in with Google" }).click();
+// });
 
-test("can create a take", async ({ page }) => {
-  await authSetup(page);
-  await page.getByRole("link", { name: "+ Add Take!" }).click();
-  await page.getByPlaceholder("Search for Movie...").click();
-  await page.getByPlaceholder("Search for Movie...").fill("die hard");
-  await page.locator("li").filter({ hasText: "Die Hard - 1988" }).click();
-  await page.getByRole("button", { name: "Genre" }).click();
-  await page.getByText("Drama").click();
-  await page.getByRole("button", { name: "Status" }).click();
-  await page.getByText("Have Seen").click();
-  await page.locator("textarea").click();
-  await page.locator("textarea").fill("Still one of the action greats...");
-  await page.getByRole("button", { name: "Add Feedback" }).click();
-  await expect(
-    page.getByRole("link", {
-      name: 'Die Hard (1988) @jason.amey\'s take: "Still one of the action greats..." Drama',
-    })
-  ).toBeVisible();
-});
+// test("can create a take", async ({ page }) => {
+//   await authSetup(page);
+//   await page.getByRole("link", { name: "+ Add Take!" }).click();
+//   await page.getByPlaceholder("Search for Movie...").click();
+//   await page.getByPlaceholder("Search for Movie...").fill("die hard");
+//   await page.locator("li").filter({ hasText: "Die Hard - 1988" }).click();
+//   await page.getByRole("button", { name: "Genre" }).click();
+//   await page.getByText("Drama").click();
+//   await page.getByRole("button", { name: "Status" }).click();
+//   await page.getByText("Have Seen").click();
+//   await page.locator("textarea").click();
+//   await page.locator("textarea").fill("Still one of the action greats...");
+//   await page.getByRole("button", { name: "Add Feedback" }).click();
+//   await expect(
+//     page.getByRole("link", {
+//       name: 'Die Hard (1988) @jason.amey\'s take: "Still one of the action greats..." Drama',
+//     })
+//   ).toBeVisible();
+// });
